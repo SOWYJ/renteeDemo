2b8cb5f85c6f7e27ad4cf38f15a6024d4a8d8c92
Spikes <412404308@qq.com>
2024-08-15 22:23:42 +0800

    租车管理系统微服务项目

---
+++ vehicle-Information/src/test/java/com/example/vehicleinformation/VehicleInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vehicleinformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
