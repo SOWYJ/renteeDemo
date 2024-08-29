716cd22a625fd1d07c6b3bbc18d139dff3e7e736
jwWillw <128061127+jwWillw@users.noreply.github.com>
2024-08-29 09:55:25 +0800

    Add files via upload
    until包

---
+++ activity-services/src/main/java/com/example/activityservices/until/UploadUntil.java
@@ -0,0 +1,41 @@
+package com.example.activityservices.until;
+
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class UploadUntil {
+
+    public static final String DOMAIN = "https://test-01234.oss-cn-wulanchabu.aliyuncs.com/";
+//
+    public static String uploadFile(MultipartFile file){
+        String oldFileName = file.getOriginalFilename();
+        String ext = "." + FilenameUtils.getExtension(oldFileName);
+        String uuid = UUID.randomUUID().toString().replace("-","");
+        String fileName = uuid + ext;
+
+        String endpoint = "http://oss-cn-wulanchabu.aliyuncs.com";
+        String accessKeyId = "LTAI5t7dS6mEugZrSLqsk1sc";
+        String accessKeySecret = "SBIw1Ogriz9Qrevs57GwoDyTuS7Fcx";
+        String bucketName = "test-01234";
+
+        OSS ossClient = new OSSClientBuilder().build(endpoint,accessKeyId,accessKeySecret);
+
+        try {
+            ossClient.putObject(bucketName,fileName,file.getInputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }finally {
+            ossClient.shutdown();
+        }
+        return DOMAIN + fileName;
+    }
+
+}
+
+
